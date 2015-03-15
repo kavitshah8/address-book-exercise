49ce4e970fa635958c94b328f236b2f1042bdb80
kavitshah8 <kavit.shah8@gmail.com>
2015-03-15 11:49:26 -0400

    creates and appends DOM using sorted data

--- mockup/index.js
+++ mockup/index.js
@@ -1,16 +1,38 @@
+var sortByFirstName = function(people){
+
+	people.sort(function(a, b){
+
+    if(a.firstName < b.firstName){
+			return -1;
+    }
+
+    if(a.firstName > b.firstName){
+			return 1;
+    }
+
+    return 0;
+
+	});
+
+};
+
+var createDOM = function (people){
+
+	for(var i = 0; i < people.length; i++){
+
+		$('.app-directory').append('<div class="app-directory-item">' + people[i].firstName + ' ' + people[i].lastName + '</div>');
+	}
+};
+
+// GET DATA FROM LOCAL SERVER, SORT THE DATA, CREATE DOM
+
 $.get('//localhost:8080/api/people', function(data){
 
-	var data = JSON.parse(data);
-	sortByFirstName(data.people);
+	var parsedData = JSON.parse(data);
+	sortByFirstName(parsedData.people);
+	createDOM(parsedData.people);
 })
 .fail(function(){
-	console.log("Something went wrong");
+	console.log('Something went wrong');
 });
 
-var sortByFirstName = function(people){
-	people.sort(function(a, b){
-    if(a.firstName < b.firstName) return -1;
-    if(a.firstName > b.firstName) return 1;
-    return 0;
-	});
-};
