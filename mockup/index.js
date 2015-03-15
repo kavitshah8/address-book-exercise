7d73defd871683d77a72de2da0df7c5e0c98c12a
kavitshah8 <kavit.shah8@gmail.com>
2015-03-15 16:42:09 -0400

    updates DOM with Education on click event

--- mockup/index.js
+++ mockup/index.js
@@ -1,4 +1,4 @@
-var sortByFirstName = function(people){
+var sortByFirstName = function (people) {
 
 	people.sort(function(a, b){
 
@@ -16,29 +16,49 @@ var sortByFirstName = function(people){
 
 };
 
-var createDOM = function (people){
+var createDOM = function (people) {
 
-	for(var i = 0; i < people.length; i++){
+	for (var i = 0; i < people.length; i++) {
 
 		var name = '<div class="app-directory-item">' + people[i].firstName + ' ' + people[i].lastName + '</div>';
 		$('.app-directory').append(name);
 	}
 };
 
-var attachEventHandler = function(people){
+var attachEventHandler = function (people) {
 
-	$('.app-directory-item').click(function(){
+	$('.app-directory-item').click(function() {
+
 		var nameStr = $(this).html();
-		var nameArray = nameStr.split(' ');
+		var nameTrim = nameStr.trim();	
+		var nameArray = nameTrim.split(' ');
 		var firstName = nameArray[0];
 
-		for(var i = 0; i < people.length; i++){
+		for (var i = 0; i < people.length; i++) {
 			
 			if (firstName === people[i].firstName) {
 
 				var name = '<h2>'+ people[i].firstName + ' ' + people[i].lastName +'</h2>';
 				$('.app-person-profile-header h2').replaceWith(name);
+				
+				for (var j = 0; j < people[i].education.length; j++ ){
 
+					var body = '<div class="app-history-item">'
+						+ '<div class="app-history-item-dates">' 
+							+ people[i].education[j].startYear + ' ' + people[i].education[j].endYear
+						+ '</div>'
+	          + '<div class="app-history-item-body">'
+							+	'<div class="app-history-item-title">'
+								+ people[i].education[j].institution
+							+ '</div>'
+							+ people[i].education[j].degree
+						+ '</div>'
+					+ '</div>';
+				}
+
+				$('.education .app-history-item').remove();
+
+				$('.education .app-section-body').append(body);
 			};
 		}
 	});
