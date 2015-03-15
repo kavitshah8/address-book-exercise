f1589603301e026a1b7306b71b8b1da933974828
kavitshah8 <kavit.shah8@gmail.com>
2015-03-15 15:21:13 -0400

    attaches click event handler to update name in the right column of DOM

--- mockup/index.js
+++ mockup/index.js
@@ -24,13 +24,33 @@ var createDOM = function (people){
 	}
 };
 
-// GET DATA FROM LOCAL SERVER, SORT THE DATA, CREATE DOM
+var attachEventHandler = function(people){
+
+	$('.app-directory-item').click(function(){
+		var nameStr = $(this).html();
+		var nameArray = nameStr.split(' ');
+		var firstName = nameArray[0];
+
+		for(var i = 0; i < people.length; i++){
+			
+			if (firstName === people[i].firstName) {
+
+				var name = '<h2>'+ people[i].firstName + ' ' + people[i].lastName +'</h2>';
+				$('.app-person-profile-header h2').replaceWith(name);
+
+			};
+		}
+	});
+};
+
+// GET DATA FROM LOCAL SERVER, SORT THE DATA, CREATE DOM, ATTACH EVENT HANDLERS
 
 $.get('//localhost:8080/api/people', function(data){
 
 	var parsedData = JSON.parse(data);
 	sortByFirstName(parsedData.people);
 	createDOM(parsedData.people);
+	attachEventHandler(parsedData.people);
 })
 .fail(function(){
 	console.log('Something went wrong');
